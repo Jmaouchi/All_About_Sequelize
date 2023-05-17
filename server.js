0340e50d0d58d128655823176cdb2f5825cfc1a7
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-05-16 23:56:01 -0500

    added an option object to freezeTable name and take out the timestamp collumns also added a alter = true, to update out table after we make some changes

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
 // require sequelize package
 const Sequelize = require('sequelize');
+// get access to the .env file
+require('dotenv').config();
 
 
 // create a connection to the database
 const sequelize = new Sequelize('sequelize-video', 'root', process.env.MYSQL, {
-  host:'localhost',
-  port:3306,
   dialect: 'mysql'
 })
 
 
 // create a users table
-const users = sequelize.define('user', {
+const User = sequelize.define('user', {
+  user_id: {
+    type: Sequelize.DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   username: {
-    type: sequelize.DataTypes.STRING,
+    type: Sequelize.DataTypes.STRING,
     allowNull: false
   },
   password: {
-    type: sequelize.DataTypes.STRING
+    type: Sequelize.DataTypes.STRING
   },
   age: {
-    type: sequelize.DataTypes.INTEGER,
+    type: Sequelize.DataTypes.INTEGER,
     defaultValue: 21
+  },
+  rocks: {
+    type: Sequelize.DataTypes.BOOLEAN,
+    defaultValue: true
   }
-})
+},
+{
+  // mysql will plurielize the tabla name by default (it will add an s at the end, so with this it will freez it and the name will stay the same) 
+  freezeTableName: true,
+  // this will remove the updatedAt and createdAt collumns 
+  timestamps: false,
+  // 
 
+});
+// this will drope all tables that exists and recreate them again
+// sequelize.sync({drop: true})
 
-users.sync().then((data) => {
+// sync the model and table to the database (connect to the database)
+User.sync({ alter : false /*this will update the table without droping it (not like force = true) */ }).then((data) => {
   console.log("table is created");
 }).catch((err) => {
   console.log(err);
 })
+
+
