21291d9870e9f19340917993c9846ff5aa8457a5
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-05-18 22:50:24 -0500

    added some data to the database using sequelize

--- server.js
+++ server.js
@@ -48,8 +48,20 @@ const User = sequelize.define('user', {
 // sync the model and table to the database (connect to the database)
 User.sync({ alter: true/*this will update the table without droping it (not like force = true) */ }).then((data) => {
   // add data to table
-  const user = User.build({ username: 'jugurta', password: '123', age:25, rocks:true });
-  return user.save()
+  return User.create({
+    username: 'jugurta',
+    password: '123',
+    age:25,
+    rocks:true 
+  });
+}).then((data) => {
+  console.log('user added to database');
+  data.username = 'ali';
+  return data.save(); // or data.destroy if we want to delete it
+}).then((data) => {
+  console.log("user updated");
+  // this data.JSON will return only the data filtred wich is username, password, age and rocks
+  console.log(data.toJSON());
 }).catch((err) => {
   console.log(err);
 })
